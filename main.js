7a05d433e9a3ddb61540e3cd39cc3cb7a838daa8
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-06-20 10:33:29 -0300

    20/06

--- main.js
+++ main.js
@@ -4,12 +4,21 @@ numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
-botoes [0].onclick = diminuiTamanho;
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumenteTamanho;
 
-function diminuiTamanho(){
-    tamanhoSenha = tamanhoSenha - 1;
+function diminuiTamanho() {
+    if(tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha - 1;
+    }
     numeroSenha.textContent = tamanhoSenha;
+}
 
+function aumenteTamanho() {
+    if (tamanhoSenha < 20){
+        tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
 
 
