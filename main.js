e9a60daaacb72111b9e017307fc4d394301e8c1b
Tamiris Ceolin <125214749+TamirisCeolin@users.noreply.github.com>
2024-08-01 18:22:51 -0300

    01/08

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumenteTamanho;
 
 function diminuiTamanho() {
-    if(tamanhoSenha > 1){
+    if (tamanhoSenha > 1) {
         //tamanhoSenha = tamanhoSenha - 1;
         tamanhoSenha--;
     }
@@ -17,7 +17,7 @@ function diminuiTamanho() {
 }
 
 function aumenteTamanho() {
-    if (tamanhoSenha < 20){
+    if (tamanhoSenha < 20) {
         //tamanhoSenha = tamanhoSenha + 1;  
         tamanhoSenha++;
     }
@@ -27,8 +27,9 @@ function aumenteTamanho() {
 
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
 
-for(i=0; i<checkbox.length; i++){
+for (i = 0; i < checkbox.length; i++) {
     checkbox[i].onclick = geraSenha;
 }
 
@@ -39,28 +40,40 @@ const simbolos = '!@%*?';
 
 geraSenha();
 
-function geraSenha(){
+function geraSenha() {
     let alfabeto = '';
-    if(checkbox[0].checked){
+    if (checkbox[0].checked) {
         alfabeto = alfabeto + letrasMaiusculas;
     }
-    if(checkbox[1].checked){
+    if (checkbox[1].checked) {
         alfabeto = alfabeto + letrasMinusculas;
     }
-    if(checkbox[2].checked){
+    if (checkbox[2].checked) {
         alfabeto = alfabeto + numeros;
     }
-    if(checkbox[3].checked){
+    if (checkbox[3].checked) {
         alfabeto = alfabeto + simbolos;
     }
 
     let senha = '';
-    for ( let i = 0; i < tamanhoSenha; i++){ 
-        let numeroAleatorio = Math.random()*alfabeto.length;
+    for (let i = 0; i < tamanhoSenha; i++) {
+        let numeroAleatorio = Math.random() * alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + alfabeto[numeroAleatorio];    
+        senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha;
+    classificaSenha();
+}
+
+function classificaSenha() {
+    forcaSenha.classList.remove('fraca', 'media','forte');
+    if (tamanhoSenha > 11) {
+        forcaSenha.classList.add('forte');
+    } else if (tamanhoSenha > 5 &&){
+        forcaSenha.classList.add('media');
+    } else if (tamanhoSenha <= 5){
+        forcaSenha.classList.add('fraca');
+    }
 }
 
 
